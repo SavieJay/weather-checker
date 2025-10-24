7814e022d55fabb7ca20b913e14571976065b035
SAVIOUR <savyj03@gmail.com>
2025-10-24 13:48:28 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './header'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='neutral-900 border'>
+        <Header />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return ( 
+        <div>
+            <div>
+                <img src="src\assets\images\logo.svg" alt="" />
+            </div>
+            <div></div>
+        </div>
+     );
+}
+ 
+export default Header;
